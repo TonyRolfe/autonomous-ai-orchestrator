f4bd3c0c5d74bdbc5e0bf87ab413705a607122a5
Anthony Rolfe <arolfe90275@uat.edu>
2025-12-10 06:22:24 -0600

    Updated main.tsx

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+/**
+ * Entry point for the React application.
+ * Renders the root App component into the DOM.
+ */
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
